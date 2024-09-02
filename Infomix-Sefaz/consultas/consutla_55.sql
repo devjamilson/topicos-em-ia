63bbb004e6f5e1d48165103b4e839d373011e13c
Jamilson Ferreira <83456401+devjamilson@users.noreply.github.com>
2024-09-02 09:30:48 -0300

    Delete Infomix-Sefaz directory

--- Infomix-Sefaz/consultas/consutla_55.sql
+++
@@ -1,9 +0,0 @@
---CONSULTA NFE (55)
-
-SELECT nfe.sqnfeletronica, hin.nrdocumento cnpj, itemnfe.dsproduto, itemnfe.vlproduto, 
-itemnfe.qtproduto, itemnfe.vlbasecalcicms, itemnfe.sgunidmedcom
-FROM tbfis_nfeletronica nfe, tbfis_itemnfe itemnfe, tbfis_hinfeletron hin 
-WHERE nfe.sqnfeletronica = itemnfe.sqnfeletronica
-AND nfe.sqemitente = hin.sqhinfeletron
-and hin.nrdocumento  in ('75315333007464','75315333008940','75315333011496','75315333014673')
-and nfe.dhemissao between '2024-04-10 00:00:00' and '2024-04-19 23:59:59'
